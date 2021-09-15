d875ca61eb21701755262dbe83cd2f9f4e71e34d
Damian <damikon8@gmail.com>
2021-09-15 13:40:46 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
